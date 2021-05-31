0f16c724a96f5b75b1eb026188169c46c286d454
Mahesh-VM <maheshvm218@gmail.com>
2021-05-31 20:07:07 +0530

    React essential training part Props.

--- cardtutorial/src/App.js
+++ cardtutorial/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import CardInfo from './components/cardInfo'
+import faker from 'faker'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CardInfo 
+      name={`${faker.name.firstName()} ${faker.name.firstName()}`} 
+      designation={faker.name.jobTitle()} profile_image={faker.image.avatar()}/>
+      <CardInfo 
+      name={`${faker.name.firstName()} ${faker.name.firstName()}`} 
+      designation={faker.name.jobTitle()} profile_image={faker.image.avatar()}/>
+      <CardInfo 
+      name={`${faker.name.firstName()} ${faker.name.firstName()}`} 
+      designation={faker.name.jobTitle()} profile_image={faker.image.avatar()}/>
     </div>
   );
 }
