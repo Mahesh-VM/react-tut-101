8bcd491bdc4dee5f5a742ca1ffae516b641b9e70
Mahesh-VM <maheshvm218@gmail.com>
2021-06-14 12:56:47 +0530

    Converting the function based component to class based.

--- cardtutorial/src/App.js
+++ cardtutorial/src/App.js
@@ -1,89 +1,91 @@
 import './App.css';
 import Card from './components/cardInfo';
 import faker from 'faker';
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import {ThemeProvider} from 'styled-components';
 import Button from './elements/button';
 
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
 
-function App() {
-  const theme = {
-    primary: 'green',
-    default: 'red',
-  }
-  
-  const changeNameHandler = () => {
-    setName(faker.name.firstName())
-  }
-  
-  const [name, setName] = useState('Enter the Name.')
-  const [toggle, setToggle] = useState(true)
-  const [cardlist, setCardlist] = useState([
-    {
-      id: faker.datatype.uuid(),
-      name: faker.name.firstName(),
-      title: faker.name.jobTitle(),
-      avatar: faker.image.avatar()
-    },
-    {
-      id: faker.datatype.uuid(),
-      name: faker.name.firstName(),
-      title: faker.name.jobTitle(),
-      avatar: faker.image.avatar()
-    },
-    {
-      id: faker.datatype.uuid(),
-      name: faker.name.firstName(),
-      title: faker.name.jobTitle(),
-      avatar: faker.image.avatar()
-    },
-    {
-      id: faker.datatype.uuid(),
-      name: faker.name.firstName(),
-      title: faker.name.jobTitle(),
-      avatar: faker.image.avatar()
-    }
-  ])
-  
-  const onDeleteHandler = (event, id) =>{
-    const tmp_card_list = [...cardlist]
-    const card_index = cardlist.findIndex(card=>card.id === id)
-    tmp_card_list.splice(card_index, 1)
-    setCardlist(tmp_card_list)
-  }
-  const setNamehandler = (event, id) => {
-    const tmp_card_list = [...cardlist]
-    const card_index = cardlist.findIndex(card=>card.id === id)
-    console.log(card_index)
-    tmp_card_list[card_index].name = event.target.value
-    setCardlist(tmp_card_list)
-  }
-  const buttonClass = ['button']
-  if (cardlist.length<2) buttonClass.push('button2');
-  else buttonClass.push('button1');
-  const toggleHandler = () => setToggle(!toggle)
-  return (
-    <ThemeProvider theme={theme}>
-    <div style={{ display: 'block' }}>
-      <div>
-        <Button color='primary' length={cardlist.length}>Toggle</Button>
-        <button className={buttonClass.join(' ')} onClick={toggleHandler}>Hide/Unhide</button>
-      </div>
-      <div>
-        {toggle? 
-        (
-          cardlist.map((card)=><Card className="flleft"
-          name={card.name} changeName={changeNameHandler} key={card.id}
-          designation={card.title} setName={(event)=>setNamehandler(event, card.id)} onDelete={()=>onDeleteHandler(card, card.id)}
-          profile_image={card.avatar}> </Card>
-          )
-        ):<p>Content hidden.</p>
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      theme:{
+        primary: 'green',
+        default: 'red',
+      },
+      cardlist:[
+        {
+          id: faker.datatype.uuid(),
+          name: faker.name.firstName(),
+          title: faker.name.jobTitle(),
+          avatar: faker.image.avatar()
+        },
+        {
+          id: faker.datatype.uuid(),
+          name: faker.name.firstName(),
+          title: faker.name.jobTitle(),
+          avatar: faker.image.avatar()
+        },
+        {
+          id: faker.datatype.uuid(),
+          name: faker.name.firstName(),
+          title: faker.name.jobTitle(),
+          avatar: faker.image.avatar()
+        },
+        {
+          id: faker.datatype.uuid(),
+          name: faker.name.firstName(),
+          title: faker.name.jobTitle(),
+          avatar: faker.image.avatar()
         }
+      ],
+      toggle: true,
+      buttonClass: ['button'],
+    }
+  }
+  
+  onDeleteHandler = (event, id) =>{
+    const tmp_card_list = [...this.state.cardlist]
+    const card_index = this.state.cardlist.findIndex(card=>card.id === id)
+    tmp_card_list.splice(card_index, 1)
+    this.setState({cardlist:tmp_card_list})
+  }
+  setNamehandler = (event, id) => {
+    const tmp_card_list = [...this.state.cardlist]
+    const card_index = this.state.cardlist.findIndex(card=>card.id === id)
+    tmp_card_list[card_index].name = event.target.value
+    this.setState({cardlist:tmp_card_list})
+  }
+  toggleHandler = () => this.setState({toggle:!this.state.toggle})
+
+  render(){
+    if (this.state.cardlist.length<2) this.state.buttonClass.push('button2');
+    else this.state.buttonClass.push('button1');
+    return (
+      <ThemeProvider theme={this.state.theme}>
+      <div style={{ display: 'block' }}>
+        <div>
+          <Button color='primary' length={this.state.cardlist.length}>Toggle</Button>
+          <button className={this.state.buttonClass.join(' ')} onClick={this.toggleHandler}>Hide/Unhide</button>
+        </div>
+        <div>
+          {this.state.toggle? 
+          (
+            this.state.cardlist.map((card)=><Card className="flleft"
+            name={card.name} key={card.id}
+            designation={card.title} setName={(event)=>this.setNamehandler(event, card.id)} 
+            onDelete={()=>this.onDeleteHandler(card, card.id)}
+            profile_image={card.avatar}> </Card>
+            )
+          ):<p>Content hidden.</p>
+          }
+        </div>
       </div>
-    </div>
-    </ThemeProvider>
-  );
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
--- cardtutorial/src/components/cardInfo.jsx
+++ cardtutorial/src/components/cardInfo.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../css/cardinfo.css'
 import '../css/w3.css'
 
 
-const Card = props => {
-    return (
-        <div className="cardholder">
-            <img src={props.profile_image} alt="avatar"></img>
-            <div>
-                <div className="namediv">
-                    <h4>{ props.name }</h4>
-                </div>
-                <input className="w3-input" type="text" onChange={props.setName} value={props.name}/>
-                <button className="button button1" onClick={props.onDelete}>Delete</button>
-                <p>{ props.designation }</p>
-                <span>{ props.children }</span>
-            </div>          
-        </div>
-    )
+class Card extends Component {
+    render (){
+        return (
+            <div className="cardholder">
+                <img src={this.props.profile_image} alt="avatar"></img>
+                <div>
+                    <div className="namediv">
+                        <h4>{ this.props.name }</h4>
+                    </div>
+                    <input className="w3-input" type="text" onChange={this.props.setName} value={this.props.name}/>
+                    <button className="button button1" onClick={this.props.onDelete}>Delete</button>
+                    <p>{ this.props.designation }</p>
+                    <span>{ this.props.children }</span>
+                </div>          
+            </div>
+        )
+    }
+    
 }
 
 export default Card;
