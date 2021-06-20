e877fd6f4467ffb73444151e55b0d4417cdc0cce
Mahesh-VM <maheshvm218@gmail.com>
2021-06-20 11:54:44 +0530

    Axios fetch results using function based componenets.

--- cardtutorial/src/App.js
+++ cardtutorial/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Card from './components/cardInfo';
 import faker from 'faker';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
+const theme = {
+  primary: 'green',
+  default: 'mango'
+}
 
 function App() {
   const changeNameHandler = () => {
@@ -11,33 +16,22 @@ function App() {
   }
   
   const [name, setName] = useState('Enter the Name.')
+  const [id, setId] = useState('search id.')
   const [toggle, setToggle] = useState(true)
   const [cardlist, setCardlist] = useState([
-    {
-      id: faker.datatype.uuid(),
-      name: faker.name.firstName(),
-      title: faker.name.jobTitle(),
-      avatar: faker.image.avatar()
-    },
-    {
-      id: faker.datatype.uuid(),
-      name: faker.name.firstName(),
-      title: faker.name.jobTitle(),
-      avatar: faker.image.avatar()
-    },
-    {
-      id: faker.datatype.uuid(),
-      name: faker.name.firstName(),
-      title: faker.name.jobTitle(),
-      avatar: faker.image.avatar()
-    },
-    {
-      id: faker.datatype.uuid(),
-      name: faker.name.firstName(),
-      title: faker.name.jobTitle(),
-      avatar: faker.image.avatar()
-    }
+    
   ])
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users').then(res=>{setCardlist(res.data)})
+  }, [])
+
+  const searchUsers = (event) =>{
+    if(event.key === 'Enter'){
+      console.log(event.target.value)
+      axios.get(`https://jsonplaceholder.typicode.com/users/${event.target.value}`).then(res=>{setCardlist([res.data])})
+    }
+  }
   
   const onDeleteHandler = (event, id) =>{
     const tmp_card_list = [...cardlist]
@@ -53,17 +47,19 @@ function App() {
     setCardlist(tmp_card_list)
   }
   const toggleHandler = () => setToggle(!toggle)
+  const setID = (event) => {setId(event.target.value)}
   return (
     <div style={{ display: 'block' }}>
       <div>
         <button className="button button1" onClick={toggleHandler}>Hide/Unhide</button>
+        <input className="w3-input" type='text' onChange={setID} value={id} onKeyPress={searchUsers}/>
       </div>
       <div>
         {toggle? 
         (
           cardlist.map((card)=><Card className="flleft"
           name={card.name} changeName={changeNameHandler} key={card.id}
-          designation={card.title} setName={(event)=>setNamehandler(event, card.id)} onDelete={()=>onDeleteHandler(card, card.id)}
+          phone={card.phone} setName={(event)=>setNamehandler(event, card.id)} onDelete={()=>onDeleteHandler(card, card.id)}
           profile_image={card.avatar}> </Card>
           )
         ):<p>Content hidden.</p>
--- cardtutorial/src/components/cardInfo.jsx
+++ cardtutorial/src/components/cardInfo.jsx
@@ -6,14 +6,13 @@ import '../css/w3.css'
 const Card = props => {
     return (
         <div className="cardholder">
-            <img src={props.profile_image} alt="avatar"></img>
             <div>
                 <div className="namediv">
                     <h4>{ props.name }</h4>
                 </div>
                 <input className="w3-input" type="text" onChange={props.setName} value={props.name}/>
                 <button className="button button1" onClick={props.onDelete}>Delete</button>
-                <p>{ props.designation }</p>
+                <p>{ props.phone }</p>
                 <span>{ props.children }</span>
             </div>          
         </div>
